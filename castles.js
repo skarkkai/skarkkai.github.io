5b60fb02d37b0f9c398cb2b6abb09db98bc62342
Samuli Kärkkäinen <samuli.karkkainen@iki.fi>
2021-04-21 20:18:50 +0300

    Update castles.js

--- castles.js
+++ castles.js
@@ -9,22 +9,22 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-/*
+
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 var cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
-*/
 
+/*
 const loader = new THREE.GLTFLoader();
 console.log("about to load", model)
 loader.load(model, function ( gltf ) {
-  console.log("adding", gltf.scene)
-  //scene.add( gltf.scene);
+  scene.add( gltf.scene);
+  console.log("added", gltf.scene)
 }, undefined, function ( error ) {
   console.error("failed loading", model, error);
 } );
-
+*/
 
 camera.position.z = 50;
 renderer.render( scene, camera );
